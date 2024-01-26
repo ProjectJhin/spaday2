86b6cd5eafb863ec9e5860d75b849541bbe5f29d
Jhin <zijianw1000@gmail.com>
2024-01-25 19:59:46 -0600

    spa day 3

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,16 +12,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "/user/add";
     }
 
     @PostMapping("")
-    public String processAddUserForm(Model model, @ModelAttribute User user, @RequestParam String verify) {
+    public String processAddUserForm(Model model, @Valid @ModelAttribute User user, Errors errors, @RequestParam String verify) {
 // add form submission handling code here
 
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+        model.addAttribute(user);
+        if (errors.hasErrors()) {
+            return "/user/add";
+        }
         if (user.getPassword().equals(verify)) {
 
             model.addAttribute("password", user.getPassword());
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,14 +1,24 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.*;
+
 public class User {
+    @NotBlank(message = "username cannot be blank")
+    @Size(min=5, max = 20, message = "username has incorrect length")
     private String username;
+    @Email
     private String email;
+    @NotBlank(message = "password cannot be blank")
     private String password;
 
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public User() {
+
     }
 
     public String getUsername() {
